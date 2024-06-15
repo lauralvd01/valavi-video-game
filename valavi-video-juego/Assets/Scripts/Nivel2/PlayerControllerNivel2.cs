d951c2ecec18e27f7043223bbd5d37ccb811c4e0
Laura Levraud <laura.levraud1@etu.univ-lorraine.fr>
2024-06-15 17:11:48 -0400

    Nivel 2 Importing a character with animations : idle, walikng and jumping. Are missing the changes in direction

--- valavi-video-juego/Assets/Scripts/Nivel2/PlayerControllerNivel2.cs
+++ valavi-video-juego/Assets/Scripts/Nivel2/PlayerControllerNivel2.cs
@@ -7,44 +7,56 @@ public class PlayerControllerNivel2 : MonoBehaviour
     public float m_speed = 10f;
     public float m_jump_speed = 10f;
 
+    private Animator m_animator;
+    private Rigidbody m_rigidbody;
     private Vector3 m_restart_position = new Vector3(-42, 3, 0);
 
     void Start()
     {
+        m_animator = GetComponent<Animator>();
+        m_rigidbody = GetComponent<Rigidbody>();
         transform.position = new Vector3(0, 3, 0);
     }
 
     void Update()
     {
         if(Input.GetKey("a") || Input.GetKey("left")){
-            transform.Translate(Vector3.left * m_speed * Time.deltaTime);
+            Vector3 velocity_vector = Vector3.left * m_speed * Time.deltaTime;
+            m_animator.SetFloat("Speed_X", velocity_vector.x);
+            transform.Translate(velocity_vector);
         }
-        if(Input.GetKey("s") || Input.GetKey("down")){
-            transform.Translate(-1 * Vector3.forward * m_speed * Time.deltaTime);
+        else if(Input.GetKey("d") || Input.GetKey("right")){
+            Vector3 velocity_vector = Vector3.right * m_speed * Time.deltaTime;
+            m_animator.SetFloat("Speed_X", velocity_vector.x);
+            transform.Translate(velocity_vector);
         }
-        if(Input.GetKey("d") || Input.GetKey("right")){
-            transform.Translate(Vector3.right * m_speed * Time.deltaTime);
+        else if(Input.GetKey("s") || Input.GetKey("down")){
+            Vector3 velocity_vector = -1 * Vector3.forward * m_speed * Time.deltaTime;
+            m_animator.SetFloat("Speed_Z", velocity_vector.z);
+            transform.Translate(velocity_vector);
         }
-        if(Input.GetKey("w") || Input.GetKey("up")){
-            transform.Translate(Vector3.forward * m_speed * Time.deltaTime);
+        else if(Input.GetKey("w") || Input.GetKey("up")){
+            Vector3 velocity_vector = Vector3.forward * m_speed * Time.deltaTime;
+            m_animator.SetFloat("Speed_Z", velocity_vector.z);
+            transform.Translate(velocity_vector);
+        }
+        else{
+            m_animator.SetFloat("Speed_X", m_rigidbody.velocity.x);
+            m_animator.SetFloat("Speed_Z", m_rigidbody.velocity.z);
         }
 
         if(Input.GetKey("space") && transform.position.y < 6){
+            m_animator.SetBool("Jump", true);
             transform.Translate(Vector3.up * m_jump_speed * Time.deltaTime);
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("Collision detected with " + collision.gameObject.name);
+        else{
+            m_animator.SetBool("Jump", false);
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Trigger detected with " + other.gameObject.name + " and " + this.gameObject.name);
         if(other.gameObject.name == "RightBorder"){
-            Debug.Log("RightBorder");
-
             transform.position = m_restart_position;
         }
     }
