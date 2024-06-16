4b1aa287400f946b7d7a9bedc308d5ab66fcf58f
Laura Levraud <laura.levraud1@etu.univ-lorraine.fr>
2024-06-16 12:24:36 -0400

    Restart and Menu onplay buttons

---
+++ valavi-video-juego/Assets/Scripts/SC_Nivel.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Nivel : MonoBehaviour
+{
+    public int nivel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RestartNivelButton() {
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.Lvlrst);
+        switch (nivel) {
+            case 1:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel1");
+                break;
+            case 2:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel2");
+                break;
+            case 3:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("Nivel3");
+                break;
+            default:
+                UnityEngine.SceneManagement.SceneManager.LoadScene("HomeScene");
+                break;
+        }
+    }
+
+    public void GoToMenuButton() {
+        AudioManager.Instance.PlaySFX(AudioManager.Instance.ITEM);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("HomeScene");
+    }
+}
